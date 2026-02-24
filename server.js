ddae4af80d10b708ecff74d80ec68f7be214286f
Ali AlDerazi <ali.alderazi0@gmail.com>
2026-02-24 11:56:45 +0300

    setup server and database connection

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config({ quiet: true })
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const session = require("express-session")
+const { MongoStore } = require("connect-mongo")
+const app = express()
+const path = require("path")
+const PORT = process.env.PORT ? process.env.PORT : 3000
+const dns = require("dns")
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+const db = require("./db")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(morgan("dev"))
+app.use(methodOverride("_method"))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+)
+
+app.get("/", (req, res) => {
+  res.send("🧑‍🍳 Mongoose Recipes is waiting for orders . . . ")
+})
+
+app.listen(PORT, () => {
+  console.log(`🍳 Mongoose Recipes Server is cooking on port ${PORT} . . .`)
+})
