75ef2eae0c3aad914e5a2d14ffa934ad951c253f
Maryam Altammam <mrymst03@gmail.com>
2026-02-25 14:24:44 +0300

    finish finallygit add .

--- server.js
+++ server.js
@@ -1,37 +1,54 @@
-require('dotenv').config({
-  quiet: true
-})
+require('dotenv').config({ quiet: true })
 const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
 const session = require('express-session')
+
 const { MongoStore } = require("connect-mongo")
-const path = require('path')
+
+const path = require("path")
+
+const middleware = require("./middleware")
+
+const authRouter = require('./routes/authRouter.js')
+const userRouter = require('./routes/userRouter.js')
+const recipeRouter = require('./routes/recipeRouter.js')
 
 const PORT = process.env.PORT ? process.env.PORT : 3000
+
+
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+
+const db = require('./db')
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
-app.use(morgan('dev'))
 app.use(methodOverride('_method'))
-app.use(session({
-  secret:process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI
+app.use(morgan('dev'))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    })
   })
-}))
+)
+app.use(middleware.passUserToView)
 
-app.get('/', (req,res) => {
-  res.send('🧑‍🍳 Mongoose Recipes is waiting for orders . . . ')
+app.use('/auth', authRouter)
+app.use('/users', userRouter)
+app.use('/recipes', recipeRouter)
+
+app.get('/', (req, res) => {
+  res.render('./index.ejs')
 })
 
-
-
-app.listen(PORT, ()=> {
-  console.log(`🥘 Mongoose Recipes Server is cooking on Port ${PORT} . . . `)
+app.listen(PORT, () => {
+  console.log(`🥘 Mongoose Recipes Server is cooking on Port ${PORT} . . .`)
 })
