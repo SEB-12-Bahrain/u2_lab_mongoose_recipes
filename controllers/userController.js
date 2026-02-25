a562da17a5593101a6bee6cbbfdcd1bbad0d583a
YUSUF-ALSHIEKH <yousifsheikh19@gmail.com>
2026-02-25 16:49:37 +0300

    complete hw

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require("../models/User")
+const Recipe = require("../models/Recipe")
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const recipes = await Recipe.find({ author: user._id })
+
+    const data = {
+      _id: user._id,
+      first: user.first,
+      last: user.last,
+      picture: user.picture,
+      recipes: recipes,
+    }
+
+    res.render("./users/profile.ejs", { user: data })
+  } catch (error) {
+    console.error("⚠️ An error has occurred finding a user!", error.message)
+  }
+}
+module.exports = {
+  getUserById,
+}
