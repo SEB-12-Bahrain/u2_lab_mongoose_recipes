60448e8bfd030b9a21b06f3fd322ceacce52e09a
Aboood105 <orcs882@gmail.com>
2026-02-26 03:51:15 +0300

    finish hw

---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config({ quiet: true })
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const session = require("express-session")
+const { MongoStore } = require("connect-mongo")
+const path = require("path")
+const app = express()
+const dns = require("dns")
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+const db = require("./db")
+
+const middleware = require("./middleware")
+const authRouter = require("./routes/authRouter.js")
+const userRouter = require("./routes/userRouter.js")
+const PORT = process.env.PORT ? process.env.PORT : 3000
+const recipeRouter = require("./routes/recipeRouter.js")
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use(morgan("dev"))
+app.use(methodOverride("_method"))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+)
+
+app.use(middleware.passUserToView)
+app.use("/auth", authRouter)
+app.use("/users", userRouter)
+app.use("/recipes", recipeRouter)
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+})
+
+app.listen(PORT, () => {
+  console.log(`🥘 Mongoose Recipes Server is cooking on Port ${PORT} . . . `)
+})
